1c84ac9300bf36e414cff26ceecb4e474d5f680c
Documenter.jl <documenter@juliadocs.github.io>
2024-12-23 09:52:19 +0000

    build based on 44a2048

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Replication_Monge_et_al_2019","category":"page"},{"location":"#Replication*Monge*et*al*2019","page":"Home","title":"ReplicationMongeetal2019","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ReplicationMongeetal2019.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Replication_Monge_et_al_2019]","category":"page"}]
+}
