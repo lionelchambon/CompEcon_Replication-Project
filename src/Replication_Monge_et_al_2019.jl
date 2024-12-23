04ba2d69f9a6fecc8a6a2e3508437ed2ee3e2773
Paulogcd <gugelmopaulo@gmail.com>
2024-12-23 12:21:53 +0100

    Testing the deployment of documentation with a minimalistic setup.

--- src/Replication_Monge_et_al_2019.jl
+++ src/Replication_Monge_et_al_2019.jl
@@ -2,4 +2,15 @@ module Replication_Monge_et_al_2019
 
 # Write your package code here.
 
+"""
+This function is a test.
+"""
+test_function() = print("Hello world")
+
+# We can also include other code files with the include function : 
+# include("other_file.jl")
+
+# We can make the functions available by using the export function to export only the chosen ones.
+# export
+
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,7 @@ using Test
 
 @testset "Replication_Monge_et_al_2019.jl" begin
     # Write your tests here.
+    
+    # Test of testing :
+    @test 1 == 1
 end
