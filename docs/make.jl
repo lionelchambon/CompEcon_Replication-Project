839cad5f69ef32082e0f1b7184af4d3518eedcb2
Lionel Chambon <lionel.chambon@sciencespo.fr>
2025-01-07 21:12:10 +0100

    Fixing links

--- docs/make.jl
+++ docs/make.jl
@@ -8,7 +8,7 @@ makedocs(;
     authors="CHAMBON Lionel, COMPÉRAT Étienne, and GUGELMO CAVALHEIRO DIAS Paulo",
     sitename="Replication_Monge_et_al_2019.jl",
     format=Documenter.HTML(;
-        canonical="https://Paulogcd.github.io/Replication_Monge_et_al_2019.jl",
+        canonical="https://Paulogcd.github.io/Replication_Monge_et_al_2019.jl/dev",
         edit_link="main",
         assets=String[],
     ),
@@ -19,6 +19,6 @@ makedocs(;
 )
 
 deploydocs(;
-    repo="github.com/Paulogcd/Replication_Monge_et_al_2019.jl",
+    repo="github.com/Paulogcd/Replication_Monge_et_al_2019.jl/dev",
     devbranch="main",
 )
